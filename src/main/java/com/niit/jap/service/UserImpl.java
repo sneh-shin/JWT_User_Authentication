4703e6c652f2fef8b5af48c71710e563e6f039d7
sneh-shin <snehashinde800@gmail.com>
2022-11-29 15:41:27 +0530

    injected UserRepository in the UserImpl.java

--- src/main/java/com/niit/jap/service/UserImpl.java
+++ src/main/java/com/niit/jap/service/UserImpl.java
@@ -5,5 +5,39 @@
  */
 package com.niit.jap.service;
 
-public class UserImpl {
+import com.niit.jap.domain.User;
+import com.niit.jap.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserImpl implements UserService {
+    private UserRepository repository;
+
+    @Autowired
+    public UserImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User getById(int id) {
+        return null;
+    }
+
+    @Override
+    public User getByEmailAndPassword(String email, String password) {
+        return null;
+    }
 }
