8d9b27088af32e7583c13943f18b22ffc1939b0e
sneh-shin <snehashinde800@gmail.com>
2022-11-29 15:31:07 +0530

    added non-parameterised and parameterised constructors,getters and setter,toString()

--- src/main/java/com/niit/jap/domain/User.java
+++ src/main/java/com/niit/jap/domain/User.java
@@ -19,4 +19,66 @@ public class User {
     private long phone;
     private String email;
     private String password;
+
+    public User() {
+    }
+
+    public User(int id, String name, long phone, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
