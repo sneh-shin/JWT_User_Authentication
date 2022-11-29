64f6ca74173eddf9a850c16050f8df26c97a6598
sneh-shin <snehashinde800@gmail.com>
2022-11-29 15:39:06 +0530

    added abstract metods in the interface

--- src/main/java/com/niit/jap/service/UserService.java
+++ src/main/java/com/niit/jap/service/UserService.java
@@ -1,4 +1,15 @@
 package com.niit.jap.service;
 
+import com.niit.jap.domain.User;
+
+import java.util.List;
+
 public interface UserService {
+    User saveUser(User user);
+
+    List<User> getAll();
+
+    User getById(int id);
+
+    User getByEmailAndPassword(String email, String password);
 }
