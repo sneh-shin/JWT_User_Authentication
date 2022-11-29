5eedadde04dbcdc44db36b8e69fb69ac030f75b7
sneh-shin <snehashinde800@gmail.com>
2022-11-29 15:35:09 +0530

    added custom method findByEmailAndPassword() in the UserRepository interface

--- src/main/java/com/niit/jap/repository/UserRepository.java
+++ src/main/java/com/niit/jap/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.niit.jap.repository;
 
-public interface UserRepository {
+import com.niit.jap.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmailAndPassword(String email, String password);
 }
